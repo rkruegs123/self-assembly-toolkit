d576a8429a327f2b33b1bb5cfe6a95ca25747cc4
Livia Guttieres <liviaguttieres@g.harvard.edu>
2024-01-29 10:25:03 -0500

    3 monomers

---
+++ check_energies/test3.py
@@ -0,0 +1,305 @@
+import matplotlib.pyplot as plt
+import pdb
+from tqdm import tqdm
+import numpy as onp
+
+import jax.numpy as jnp
+from jax_md import rigid_body, energy, util, space, dataclasses
+import optax
+from jax import jit, grad, vmap, value_and_grad, hessian, jacfwd, jacrev, random, lax
+import potentials
+from jax_transformations3d import jax_transformations3d as jts
+
+from jax.config import config
+config.update("jax_enable_x64", True)
+
+
+"""
+Notes:
+- we will consider only the dimer system
+"""
+
+
+vertex_species = 0
+n_species = 7
+
+# Helper functions
+
+euler_scheme = "sxyz"
+
+def convert_to_matrix(mi):
+    """
+    Convert a set x,y,z,alpha,beta,gamma into a jts transformation matrix
+    """
+    T = jts.translation_matrix(mi[:3])
+    R = jts.euler_matrix(mi[3], mi[4], mi[5], axes=euler_scheme)
+    return jnp.matmul(T,R)
+
+
+
+# Define the dimer
+num_building_blocks = 3
+
+
+a = 1 # distance of the center of the spheres from the BB COM
+b = .3 # distance of the center of the patches from the BB COM
+shape1 = onp.array([[-a, 0., b], # first patch
+    [-a, b*onp.cos(onp.pi/6.), -b*onp.sin(onp.pi/6.)], # second patch
+    [-a, -b*onp.cos(onp.pi/6.), -b*onp.sin(onp.pi/6.)], 
+    [0., 0., a],
+    [0., a*onp.cos(onp.pi/6.), -a*onp.sin(onp.pi/6.)], # second sphere
+    [0., -a*onp.cos(onp.pi/6.), -a*onp.sin(onp.pi/6.)], # third sphere
+    [a, 0., b], # first patch
+    [a, b*onp.cos(onp.pi/6.), -b*onp.sin(onp.pi/6.)], # second patch
+    [a, -b*onp.cos(onp.pi/6.), -b*onp.sin(onp.pi/6.)] # third patch
+])
+
+# these are the positions of the spheres within the building block
+shape2 = shape1.copy()
+shape3 = shape1.copy()
+
+trimer_shapes = jnp.array([shape1, shape2, shape3])
+dimer_shapes = jnp.array([shape1, shape2])
+
+separation = 2.
+noise = 1e-14
+trimer_rb = jnp.array([-separation, noise, 0, 0, 0, 0,   
+                     0, 0, 0, 0, 0, 0,                
+                     separation, noise, 0, 0, 0, 0],      
+                    dtype=jnp.float64)
+
+dimer_rb = jnp.array([-separation/2.0, noise, 0, 0, 0, 0,
+                     separation/2.0, 0, 0, 0, 0, 0], dtype=jnp.float64)
+
+
+vertex_radius = a
+patch_radius = 0.2*a
+
+
+def get_positions(q, ppos):
+    Mat = []
+    for i in range(len(ppos)):  
+        qi = i * 6
+        Mat.append(convert_to_matrix(q[qi:qi+6]))
+
+    real_ppos = []
+    for i, mat in enumerate(Mat):
+        real_ppos.append(jts.matrix_apply(mat, ppos[i]))
+
+    return real_ppos 
+
+
+
+# Setup soft-sphere repulsion between table values
+small_value = 1e-12 
+
+rep_A_table = onp.full((n_species, n_species), small_value)  
+rep_A_table[vertex_species, vertex_species] = 500.0  
+rep_A_table = jnp.array(rep_A_table)
+
+rep_rmax_table = onp.full((n_species, n_species), 2*vertex_radius)  
+rep_rmax_table = jnp.array(rep_rmax_table)
+
+rep_stg_alpha = 2.5
+rep_alpha_table = onp.full((n_species, n_species), rep_stg_alpha)
+rep_alpha_table = jnp.array(rep_alpha_table)
+
+
+
+# Setup morse potential between table values
+default_weak_eps = small_value
+morse_eps_table = onp.full((n_species, n_species), default_weak_eps)
+default_strong_eps = 10.0
+morse_eps_table[onp.array([1, 2, 3]), onp.array([1, 2, 3])] = default_strong_eps
+morse_eps_table = jnp.array(morse_eps_table)
+
+morse_weak_alpha = 1e-12 
+morse_alpha_table = onp.full((n_species, n_species), morse_weak_alpha)
+morse_strong_alpha = 5.0
+morse_alpha_table[onp.array([2, 3, 4, 5]), onp.array([3, 2, 5, 4])] = morse_strong_alpha
+morse_alpha_table = jnp.array(morse_alpha_table)
+
+
+def pairwise_repulsion(ipos, jpos, i_species, j_species):
+  
+    rep_rmax = rep_rmax_table[i_species, j_species]
+    rep_a = rep_A_table[i_species, j_species]
+    rep_alpha = rep_alpha_table[i_species, j_species]
+    dr = space.distance(ipos - jpos)
+
+    return potentials.repulsive(dr, rmin=0, rmax=rep_rmax, A=rep_a, alpha=rep_alpha)
+               
+                     
+
+def pairwise_morse(ipos, jpos, i_species, j_species):
+                     
+    morse_d0 = morse_eps_table[i_species, j_species]
+    morse_alpha = morse_alpha_table[i_species, j_species]
+  
+    morse_r0 = 0.0                                   
+    morse_rcut = 8. / morse_alpha + morse_r0
+    dr = space.distance(ipos - jpos)
+                     
+    return potentials.morse_x(dr, rmin=morse_r0, rmax=morse_rcut, D0=morse_d0, 
+                   alpha=morse_alpha, r0=morse_r0, ron=morse_rcut/2.)                    
+                                      
+                     
+                     
+                     
+def trimer_energy(q, pos, species):
+    
+    positions = get_positions(q, pos)
+    
+    
+    pos1 = positions[0] 
+    pos2 = positions[1]
+    pos3 = positions[2] 
+                 
+    species1 = species[:3]  
+    species2 = species[3:6]
+    species3 = species[6:12]
+    species1 = onp.repeat(species1, 3) 
+    species2 = onp.repeat(species2, 3)
+    species3 = onp.repeat(species3, 3)
+    
+
+    morse_func = vmap(vmap(pairwise_morse, in_axes=(None, 0, None, 0)), in_axes=(0, None, 0, None))
+    tot_energy = jnp.sum(morse_func(pos1, pos2, species1, species2))
+    tot_energy += jnp.sum(morse_func(pos1, pos3, species1, species3))
+    tot_energy += jnp.sum(morse_func(pos2, pos3, species2, species3))
+    
+    inner_rep = vmap(pairwise_repulsion, in_axes=(None, 0, None, 0))
+    rep_func = vmap(inner_rep, in_axes=(0, None, 0, None))
+    tot_energy += jnp.sum(rep_func(pos1, pos2, species1, species2)) 
+    tot_energy += jnp.sum(rep_func(pos1, pos3, species1, species3))  
+    tot_energy += jnp.sum(rep_func(pos2, pos3, species2, species3))  
+
+    return tot_energy  
+
+def dimer_energy(q, pos, species):
+    
+    positions = get_positions(q, pos)
+    
+    pos1 = positions[0] 
+    pos2 = positions[1] 
+                 
+    species1 = species[:3]  
+    species2 = species[3:]
+    species1 = onp.repeat(species1, 3) 
+    species2 = onp.repeat(species2, 3)
+    
+
+    morse_func = vmap(vmap(pairwise_morse, in_axes=(None, 0, None, 0)), in_axes=(0, None, 0, None))
+    tot_energy = jnp.sum(morse_func(pos1, pos2, species1, species2))
+    
+    inner_rep = vmap(pairwise_repulsion, in_axes=(None, 0, None, 0))
+    rep_func = vmap(inner_rep, in_axes=(0, None, 0, None))
+    tot_energy += jnp.sum(rep_func(pos1, pos2, species1, species2))               
+
+    return tot_energy  
+
+
+def get_energy(q, pos, species):
+    Nbb = len(pos)
+
+    # Define a function for the Nbb == 3 case
+    def trimer_case(_):
+        return trimer_energy(q, pos, species)
+
+    # Define a function for the Nbb == 2 case
+    def dimer_case(_):
+        return dimer_energy(q, pos, species)
+
+    # Use lax.cond to conditionally execute the correct function
+    energy = lax.cond(Nbb == 3, trimer_case, dimer_case, None)
+
+    return energy
+
+
+
+    
+def add_variables(ma, mb):
+    """
+    given two vectors of length (6,) corresponding to x,y,z,alpha,beta,gamma,
+    convert to transformation matrixes, 'add' them via matrix multiplication,
+    and convert back to x,y,z,alpha,beta,gamma
+
+    note: add_variables(ma,mb) != add_variables(mb,ma)
+    """
+
+    Ma = convert_to_matrix(ma)
+    Mb = convert_to_matrix(mb)
+    Mab = jnp.matmul(Mb,Ma)
+    trans = jnp.array(jts.translation_from_matrix(Mab))
+    angles = jnp.array(jts.euler_from_matrix(Mab, euler_scheme))
+
+    return jnp.concatenate((trans, angles))
+
+def add_variables_all(mas, mbs):
+    """
+    Given two vectors of length (6*n,), 'add' them per building block according
+    to add_variables().
+    """
+
+    mas_temp = jnp.reshape(mas, (mas.shape[0] // 6, 6))
+    mbs_temp = jnp.reshape(mbs, (mbs.shape[0] // 6, 6))
+
+    return jnp.reshape(vmap(add_variables, in_axes=(0, 0))(
+        mas_temp, mbs_temp), mas.shape)
+
+
+def hess(energy_fn, q, pos, species):
+    
+    H = hessian(energy_fn)(q, pos, species)
+    evals, evecs = jnp.linalg.eigh(H)
+    return evals, evecs
+
+
+def get_zvib(energy_fn, q, pos, species):
+    evals, evecs = hess(energy_fn, q, pos, species)
+    zvib = jnp.prod(jnp.sqrt(2.*jnp.pi/(jnp.abs(evals[6:])+1e-12)))
+    return zvib
+
+def get_zrot(energy_fn, q, pos, species, seed=0, nrandom=100000):
+
+    key = random.PRNGKey(seed)
+    Nbb = len(pos) #to be changed
+
+    evals, evecs = hess(energy_fn, q, pos, species)
+
+
+    def set_nu_random(key):
+        quat = jts.random_quaternion(None, key)
+        angles = jnp.array(jts.euler_from_quaternion(quat, euler_scheme))
+        nu0 = jnp.full((Nbb * 6,), 0.)
+        return nu0.at[3:6].set(angles)
+
+ 
+    def ftilde(nu):
+        q_tilde = jnp.matmul(evecs.T[6:].T, nu[6:])
+        nu_tilde = jnp.reshape(jnp.array([nu[:6] for _ in range(Nbb)]), nu.shape) 
+        return add_variables_all(q_tilde, nu_tilde)
+    
+    f = ftilde
+
+    key, *splits = random.split(key, nrandom + 1)
+    nus = vmap(set_nu_random)(jnp.array(splits))
+
+
+    nu_fn = lambda nu: jnp.abs(jnp.linalg.det(jacfwd(f)(nu)))
+
+    Js = vmap(nu_fn)(nus)
+
+
+    J = jnp.mean(Js)
+    Jtilde = 8.0 * (jnp.pi**2) * J
+    
+    return Jtilde
+
+target_species = [1,0,2,3,0,4,4,0,3] 
+print(trimer_energy(trimer_rb, trimer_shapes, target_species))
+print(get_zrot(trimer_energy, trimer_rb, trimer_shapes, target_species))   
+
+
+                     
+                     
